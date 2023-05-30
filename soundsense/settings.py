0b28369dca47a7fe0b4a5d9460f37d828c4a2480
Lousy_malayali <neerajvb@gmail.com>
2023-05-30 13:15:46 +0530

    gitignore and settings update

--- soundsense/settings.py
+++ soundsense/settings.py
@@ -74,14 +74,14 @@ WSGI_APPLICATION = 'soundsense.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'soundsense',
@@ -90,7 +90,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306'
     }
-}'''
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
